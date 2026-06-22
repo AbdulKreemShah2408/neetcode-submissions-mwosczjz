84923284dd91455d156ae271bb74cb99b973463c
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2026-06-22 08:27:48 +0500

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/perfect-squares/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int>dp(n+1,n);
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+          for(int j=1;j*j<=i;j++){
+            int saqure=j*j;
+            dp[i]=min(dp[i],dp[i-saqure]+1);
+          }
+        }
+        return dp[n];
+    }
+};
