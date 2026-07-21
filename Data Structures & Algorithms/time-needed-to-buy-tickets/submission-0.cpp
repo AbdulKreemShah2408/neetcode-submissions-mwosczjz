46c054103082338033a6fd67a0cc37efcaa72c2b
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2026-07-21 13:32:38 +0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/time-needed-to-buy-tickets/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int totalTime = 0;
+        
+        for (int i = 0; i < tickets.size(); i++) {
+           
+            if (i <= k) {
+                totalTime += min(tickets[i], tickets[k]);
+            } 
+           
+            else {
+                totalTime += min(tickets[i], tickets[k] - 1);
+            }
+        }
+        
+        return totalTime;
+    }
+};
