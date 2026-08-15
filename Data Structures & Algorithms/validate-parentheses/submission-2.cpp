677d6a57018e95a568cfb178e1ac2b0d3a822e3e
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2026-08-15 14:48:38 +0500

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(ch);
+            }else{
+                if(!st.empty()){
+                    if((ch==')' && st.top()=='(') || (ch=='}' && st.top()=='{') || (ch==']' && st.top()=='[') ) st.pop();
+              else{
+                return false;
+              }      
+
+                }else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+
+    }
+};
